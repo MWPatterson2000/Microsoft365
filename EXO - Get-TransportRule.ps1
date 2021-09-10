a4d73821eeea847eec6ef5bdfab05defe81e04da
Mike Patterson <m.w.patterson@live.com>
2021-09-10 14:39:57 -0400

    Create EXO - Get-TransportRule.ps1

---
+++ EXO - Get-TransportRule.ps1
@@ -0,0 +1,38 @@
+<#
+    .SYNOPSIS
+    Exports all Exchange Online Client Access Rules  
+    
+    .DESCRIPTION
+    Exports all Exchange Online Client Access Rules into a CSV file in c:\Temp
+	Requires M365 account account with atleast Exchange Administrator role
+    	Dependant on Exchange Online PowerShell module
+    
+    #>
+    
+# Export File
+$exportFile = "C:\Temp\$(Get-Date -Format yyyy-MM-dd-HH-mm) - EXO - TransportRule.csv"
+
+Get-TransportRule | Select-Object * | Export-Csv -Path $exportFile -Encoding utf8 -NoTypeInformation
+
+<#
+#Get Conditional Access Policies   
+$tempAR = @(Get-TransportRule) | Select-Object *
+
+$Policies = @()
+
+# Build Export Array
+$count = @(($tempAR).Displayname).Count
+$count = $count - 1
+for ($num = 0 ; $num -le $count ; $num++) {
+    #$temp = (($tempAR[$num]).ipRanges).cidrAddress -join ','
+    $Policies += New-Object PSobject -Property @{
+        "Name"  = ($tempAR[$num]).Name
+        "Priority" = ($tempAR[$num]).Priority
+        "Enabled"  = ($tempAR[$num]).Enabled
+        "DatacenterAdminsOnly" = ($tempAR[$num]).DatacenterAdminsOnly
+    }
+}
+
+# Export Data
+$Policies | export-csv -Path $exportFile -NoTypeInformation
+#>
